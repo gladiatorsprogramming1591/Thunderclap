a8f13725a8cfa1d7bd28919199ce5b0a6eac3322
Jeff Tuttle <jefft138@gmail.com>
2020-01-25 20:34:19 -0500

    Fix compilation errors

--- InfiniteRecharge2020-PBot/src/main/java/frc/robot/RobotContainer.java
+++ InfiniteRecharge2020-PBot/src/main/java/frc/robot/RobotContainer.java
@@ -10,12 +10,9 @@ package frc.robot;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import frc.robot.commands.ExampleCommand;
 import frc.robot.commands.IntakeStop;
 import frc.robot.commands.IntakeOn;
 import frc.robot.commands.IntakeReverse;
-import frc.robot.subsystems.ElevatorSubsystem;
-import frc.robot.subsystems.ExampleSubsystem;
 import frc.robot.subsystems.IntakeSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.Button;
@@ -33,7 +30,7 @@ public class RobotContainer {
 
   
   // The robot's commands are defined here...
-  // private final ExampleCommand m_autoCommand = new ExampleCommand(m_exampleSubsystem);
+  private final IntakeOn m_autoCommand = new IntakeOn(m_intakeSubsystem);
   
 
   // The driver's controller
