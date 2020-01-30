d7cc3ce76e50adb8869cb258b14d0f281e1c9730
Trevor <tr3v0r@outlook.com>
2020-01-30 16:34:02 -0500

    started shooter.

--- InfiniteRecharge2020-PBot/src/main/java/frc/robot/commands/ShooterOn.java
+++ InfiniteRecharge2020-PBot/src/main/java/frc/robot/commands/ShooterOn.java
@@ -13,7 +13,7 @@ import edu.wpi.first.wpilibj2.command.CommandBase;
 /**
  * An example command that uses an example subsystem.
  */
-public class ExampleCommand extends CommandBase {
+public class ShooterOn extends CommandBase {
   @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
   private final ShooterSubsystem m_subsystem;
 
@@ -22,7 +22,7 @@ public class ExampleCommand extends CommandBase {
    *
    * @param subsystem The subsystem used by this command.
    */
-  public ExampleCommand(ShooterSubsystem subsystem) {
+  public ShooterOn(ShooterSubsystem subsystem) {
     m_subsystem = subsystem;
     // Use addRequirements() here to declare subsystem dependencies.
     addRequirements(subsystem);
