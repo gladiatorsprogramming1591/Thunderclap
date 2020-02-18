ea07602c78a8d4ca06f8e055f4a03ecc695101e2
Andrew DelRegno <andrewenzodzhang2015@gmail.com>
2020-02-18 13:01:24 -0500

    Fixed Stupid Mistake accidentally forgot to edit stopperandshooter on to new name

--- InfiniteRecharge2020-PBot/src/main/java/frc/robot/commands/StopperandShooterOn.java
+++ InfiniteRecharge2020-PBot/src/main/java/frc/robot/commands/StopperandShooterOn.java
@@ -14,19 +14,18 @@ import frc.robot.subsystems.IntakeSubsystem;
 /**
  * A complex auto command that drives forward, releases a hatch, and then drives backward.
  */
-public class IntakeandSuckerOn extends ParallelCommandGroup {
+public class StopperandShooterOn extends ParallelCommandGroup {
   /**
-   * Creates a new Command Group.
-   * There are 4 types of command groups:
-   * SequentialCommandGroup - first command is executed, then the second, etc. and so on until the list finishes
-   * ParallelCommandGroup - ends commands when they finish
-   * ParallelRaceGroup - ends as soon as any command in the group ends
+   * Creates a new Command Group. There are 4 types of command groups:
+   * SequentialCommandGroup - first command is executed, then the second, etc. and
+   * so on until the list finishes ParallelCommandGroup - ends commands when they
+   * finish ParallelRaceGroup - ends as soon as any command in the group ends
    * ParallelDeadlineGroup - ends when a specific command (the “deadline”) ends
    *
    * @param subsystem1 The subsystem this command will run on
    * @param subsystem2 The subsystem this command will run on
    */
-  public IntakeandSuckerOn(IntakeSubsystem intakeSubsystem, HopperSubsystem hopperSubsystem) {
+  public StopperandShooterOn(IntakeSubsystem intakeSubsystem, HopperSubsystem hopperSubsystem) {
     addCommands(
         // Example 1
         new IntakeOn(intakeSubsystem),
