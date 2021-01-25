e7e4bec2bd4e9edf45a58d13d57ad02a171008ca
Dylan Tompkins <dylanltompkins@hotmail.com>
2021-01-25 15:23:54 -0500

    better comments

--- Thunderclap/src/main/java/frc/robot/commands/AutonomousCommands/DriveDistance/DriveCM.java
+++ Thunderclap/src/main/java/frc/robot/commands/AutonomousCommands/DriveDistance/DriveCM.java
@@ -3,12 +3,15 @@ package frc.robot.commands.AutonomousCommands.DriveDistance;
 import frc.robot.Constants;
 import frc.robot.subsystems.DriveTrain;
 
+/**
+ * Command the robot to drive forward or backwards a certain amount of centimeters (in a straight line).
+ */
 public class DriveCM extends DriveEncoder{
     /**
-     * Construct a DriveEncoder with centimeters translated into rotations
+     * Construct a DriveEncoder with centimeters translated into rotations.
      * 
-     * @param driveTrain The driveTrain subsystem that DriveEncoder will use
-     * @param cm The distance in centimeters the robot should travel
+     * @param driveTrain The driveTrain subsystem that DriveEncoder will use.
+     * @param cm The distance in centimeters the robot should travel (use a negative value to go backwards).
      */
     public DriveCM(DriveTrain driveTrain, double cm) {
         super(driveTrain, cm / Constants.kWheelCircumferenceCM);
