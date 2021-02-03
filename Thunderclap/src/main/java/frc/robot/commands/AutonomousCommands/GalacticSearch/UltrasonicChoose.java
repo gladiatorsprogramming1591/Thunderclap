bd9430f2a57f1a65d110546a6774bfba23259294
Dylan Tompkins <dylanltompkins@hotmail.com>
2021-02-03 15:43:49 -0500

    init ultrasonic choosing

---
+++ Thunderclap/src/main/java/frc/robot/commands/AutonomousCommands/GalacticSearch/UltrasonicChoose.java
@@ -0,0 +1,18 @@
+package frc.robot.commands.AutonomousCommands.GalacticSearch;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.DriveTrainC;
+
+public class UltrasonicChoose extends CommandBase{
+    private DriveTrainC m_DriveTrainC;
+    
+    
+    public UltrasonicChoose(DriveTrainC driveTrainC) {
+        m_DriveTrainC = driveTrainC;
+    }
+
+    @Override
+    public void initialize() {
+
+    }
+}
