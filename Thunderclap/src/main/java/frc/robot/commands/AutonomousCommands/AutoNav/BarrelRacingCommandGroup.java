f19e748f041d671112c4fd37ec4f56df86ef2be8
Dylan Tompkins <dylanltompkins@hotmail.com>
2021-03-02 16:19:49 -0500

    barrel racing

--- Thunderclap/src/main/java/frc/robot/commands/AutonomousCommands/AutoNav/BarrelRacingCommandGroup.java
+++ Thunderclap/src/main/java/frc/robot/commands/AutonomousCommands/AutoNav/BarrelRacingCommandGroup.java
@@ -3,7 +3,7 @@ package frc.robot.commands.AutonomousCommands.AutoNav;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.commands.AutonomousCommands.DriveDistance.DriveInches;
 import frc.robot.subsystems.DriveTrainC;
-import frc.robot.commands.AutonomousCommands.Rotation.TurnToDegrees;
+import frc.robot.commands.AutonomousCommands.Rotation.PIDTurnToDegrees;
 
 /**
  * Robot drives the Barrel Racing Path from the AutoNav challenge
@@ -17,13 +17,43 @@ public class BarrelRacingCommandGroup extends SequentialCommandGroup{
      */
     public BarrelRacingCommandGroup(DriveTrainC driveTrain) {
         addCommands(
-            new DriveInches(driveTrain, 126),
-            new TurnToDegrees(driveTrain, -50, false),
+            new DriveInches(driveTrain, 180),
+            new PIDTurnToDegrees(driveTrain, 90, false),
+
+            new DriveInches(driveTrain, 80),
+            new PIDTurnToDegrees(driveTrain, 90, false),
+
+            new DriveInches(driveTrain, 80),
+            new PIDTurnToDegrees(driveTrain, 90, false),
+            
+            new DriveInches(driveTrain, 70),
+            new PIDTurnToDegrees(driveTrain, 90, false),
+            
+            new DriveInches(driveTrain, 150),
+            new PIDTurnToDegrees(driveTrain, -90, false),
+            
+            new DriveInches(driveTrain, 80),
+            new PIDTurnToDegrees(driveTrain, -90, false),
+            
+            new DriveInches(driveTrain, 80),
+            new PIDTurnToDegrees(driveTrain, -90, false),
+            
             new DriveInches(driveTrain, 85),
-            new TurnToDegrees(driveTrain, 98.14, false),
-            new DriveInches(driveTrain, 67.08),
-            new TurnToDegrees(driveTrain, -10, false),
-            new DriveInches(driveTrain, 66)
+            new PIDTurnToDegrees(driveTrain, -90, false),
+            
+            new DriveInches(driveTrain, 150),
+            new PIDTurnToDegrees(driveTrain, 90, false),
+            
+            new DriveInches(driveTrain, 80),
+            new PIDTurnToDegrees(driveTrain, 90, false),
+            
+            new DriveInches(driveTrain, 80),
+            new PIDTurnToDegrees(driveTrain, 90, false),
+            
+            new DriveInches(driveTrain, 90),
+            new PIDTurnToDegrees(driveTrain, -90, false),
+            
+            new DriveInches(driveTrain, 210)
         );
     }
 }
