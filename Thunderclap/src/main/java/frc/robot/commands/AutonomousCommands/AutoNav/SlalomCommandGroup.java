11ad5f7dd72bfd41165d2a60fc7908aa6b610052
Dylan Tompkins <dylanltompkins@hotmail.com>
2021-03-02 16:30:44 -0500

    slalom

--- Thunderclap/src/main/java/frc/robot/commands/AutonomousCommands/AutoNav/SlalomCommandGroup.java
+++ Thunderclap/src/main/java/frc/robot/commands/AutonomousCommands/AutoNav/SlalomCommandGroup.java
@@ -3,7 +3,7 @@ package frc.robot.commands.AutonomousCommands.AutoNav;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.commands.AutonomousCommands.DriveDistance.DriveInches;
 import frc.robot.subsystems.DriveTrainC;
-import frc.robot.commands.AutonomousCommands.Rotation.TurnToDegrees;
+import frc.robot.commands.AutonomousCommands.Rotation.PIDTurnToDegrees;
 
 /**
  * Robot drives the Slalom Path from the AutoNav challenge
@@ -17,13 +17,37 @@ public class SlalomCommandGroup extends SequentialCommandGroup{
      */
     public SlalomCommandGroup(DriveTrainC driveTrain) {
         addCommands(
-            new DriveInches(driveTrain, 126),
-            new TurnToDegrees(driveTrain, -50, false),
-            new DriveInches(driveTrain, 85),
-            new TurnToDegrees(driveTrain, 98.14, false),
-            new DriveInches(driveTrain, 67.08),
-            new TurnToDegrees(driveTrain, -10, false),
-            new DriveInches(driveTrain, 66)
+            new DriveInches(driveTrain, 60),
+            new PIDTurnToDegrees(driveTrain, -90, false),
+
+            new DriveInches(driveTrain, 30),
+            new PIDTurnToDegrees(driveTrain, 90, false),
+
+            new DriveInches(driveTrain, 180),
+            new PIDTurnToDegrees(driveTrain, 90, false),
+            
+            new DriveInches(driveTrain, 30),
+            new PIDTurnToDegrees(driveTrain, -90, false),
+            
+            new DriveInches(driveTrain, 60),
+            new PIDTurnToDegrees(driveTrain, -90, false),
+            
+            new DriveInches(driveTrain, 30),
+            new PIDTurnToDegrees(driveTrain, -90, false),
+            
+            new DriveInches(driveTrain, 60),
+            new PIDTurnToDegrees(driveTrain, -90, false),
+            
+            new DriveInches(driveTrain, 30),
+            new PIDTurnToDegrees(driveTrain, 90, false),
+            
+            new DriveInches(driveTrain, 180),
+            new PIDTurnToDegrees(driveTrain, 90, false),
+            
+            new DriveInches(driveTrain, 30),
+            new PIDTurnToDegrees(driveTrain, -90, false),
+            
+            new DriveInches(driveTrain, 60)
         );
     }
 }
