ae34a759c33ad520dd23d68808dde0b0615b7b51
Gladiators Driveteam <gladiatorsprogramming1591@gmail.com>
2021-03-10 17:01:23 -0500

    bounce working

--- Thunderclap/src/main/java/frc/robot/commands/AutonomousCommands/AutoNav/BounceCommandGroup.java
+++ Thunderclap/src/main/java/frc/robot/commands/AutonomousCommands/AutoNav/BounceCommandGroup.java
@@ -13,6 +13,7 @@ import frc.robot.commands.IntakeArmCommands.RaiseArm;
  * Robot drives the Bounce Path from the AutoNav challenge
  */
 public class BounceCommandGroup extends SequentialCommandGroup{
+    private final double hitConesLess = 20;
 
     /**
      * Create a new BounceCommandGroup SequentialCommand Group.
@@ -26,34 +27,34 @@ public class BounceCommandGroup extends SequentialCommandGroup{
             new LowerArm(intakeArm),
 
             new DriveInches(driveTrain, 60),
-            new PIDTurnToDegrees(driveTrain, -90, false),
+            new PIDTurnToDegrees(driveTrain, 90, true),
 
-            new DriveInches(driveTrain, -60),
-
-            new DriveInches(driveTrain, 60),
-            new PIDTurnToDegrees(driveTrain, 90, false),
+            new DriveInches(driveTrain, 60 - hitConesLess),
             
-            new DriveInches(driveTrain, 30),
-            new PIDTurnToDegrees(driveTrain, 90, false),
+            new DriveInches(driveTrain, -64 + hitConesLess),
+            new PIDTurnToDegrees(driveTrain, 180, true),
             
-            new DriveInches(driveTrain, 60),
-            new PIDTurnToDegrees(driveTrain, -90, false),
+            new DriveInches(driveTrain, 32),
+            new PIDTurnToDegrees(driveTrain, 270, true),
             
             new DriveInches(driveTrain, 60),
-            new PIDTurnToDegrees(driveTrain, -90, false),
+            new PIDTurnToDegrees(driveTrain, 180, true),
             
-            new DriveInches(driveTrain, 120),
+            new DriveInches(driveTrain, 58),
+            new PIDTurnToDegrees(driveTrain, 90, true),
             
-            new DriveInches(driveTrain, -120),
-            new PIDTurnToDegrees(driveTrain, 90, false),
+            new DriveInches(driveTrain, 120 - hitConesLess),
+            
+            new DriveInches(driveTrain, -124 + hitConesLess),
+            new PIDTurnToDegrees(driveTrain, 180, true),
             
             new DriveInches(driveTrain, 90),
-            new PIDTurnToDegrees(driveTrain, -90, false),
+            new PIDTurnToDegrees(driveTrain, 90, true),
             
-            new DriveInches(driveTrain, 120),
+            new DriveInches(driveTrain, 120 - hitConesLess),
             
-            new DriveInches(driveTrain, -60),
-            new PIDTurnToDegrees(driveTrain, 90, false),
+            new DriveInches(driveTrain, -64 + hitConesLess),
+            new PIDTurnToDegrees(driveTrain, 180, true),
             
             new DriveInches(driveTrain, 60),
 
